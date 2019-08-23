4e235ffc2c6e18979521a255db67e40d40e7c17e
Brendo Maciel <maciel.brendoo@gmail.com>
2019-08-23 01:06:12 -0300

    Correção na rota backend Finalizado frontend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,6 @@ routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 
 routes.post('/devs/:devId/likes', LikeController.store);
-routes.post('/devs/:devId/likes', DislikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
